dca50ee50db117ee9c9ec02ad3f28c78a29518d3
sania-dolat <120238420+sania-dolat@users.noreply.github.com>
2024-06-24 22:17:48 +0330

    UTM class completed
    done with the run , findRule and print method of the UTM class

--- InputTape.java
+++ InputTape.java
@@ -1,31 +1,45 @@
+import java.util.Arrays;
+
 public class InputTape {
-    //Make input infinite
+    // Make input infinite
     private String[] inputTape;
     private int size;
-    public InputTape(String[] input){
+
+    public InputTape(String[] input) {
         inputTape = input;
-        size=input.length;
+        size = input.length;
     }
-    //returns new head
-    public int replace(int i,String replace){
-        if(i>=this.size){
-            String[] tape =new String[size*3];
+
+    // returns new head
+    public int replace(int i, String replace) {
+        if (i >= this.size || i < 0) {
+            String[] tape = new String[size * 3];
             int j;
-            for(j=0 ; j<size;j++){
-                tape[j]="1";
-                tape[j+size]=inputTape[j];
-                tape[j+size<<1]="1";
+            for (j = 0; j < size; j++) {
+                tape[j] = "1";
+                tape[j + size] = inputTape[j];
+                tape[j + size * 2] = "1";
             }
-            this.size= 3*size;
-            inputTape=tape;
-            i+=j;
+            this.size = 3 * size;
+            inputTape = tape;
+            i += j;
         }
-        inputTape[i]=replace;
+        inputTape[i] = replace;
         return i;
     }
 
-    public String get(int i){
-        if(i>=size) return "1";
+    public String get(int i) {
+        if (i >= size)
+            return "1";
         return inputTape[i];
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void print() {
+        System.out.println("The input tape with size of " + this.size + " : "
+                + Arrays.toString(this.inputTape));
     }
 }
--- UTM.java
+++ UTM.java
@@ -1,31 +1,79 @@
+import java.util.Arrays;
+
 /**
  * Universal Turing machine
  */
 public class UTM {
-    private InputTape input;
+    private String[] inputs;
     private String[] state;
-    private String[] description;
+    private String[] descriptions;
 
-    public UTM(){
-        input=null;
-        state=null;
-        description=null;
+    public UTM() {
+        this.inputs = null;
+        this.state = null;
+        this.descriptions = null;
     }
-    //Run a binary turing machine
-    public void run(BTM turing){
+
+    // public UTM(String[] inputs , String[] state , String[] descriptions){
+    // this.inputs = inputs;
+    // this.state = state;
+    // this.descriptions = descriptions;
+    // }
+
+    // Run a binary turing machine
+    public void run(BTM turing) {
+        this.descriptions = Arrays.copyOf(turing.getRules(), turing.getRules().length);
+        this.inputs = turing.convert(this.inputs);
+        InputTape inputs = new InputTape(this.inputs);
+        this.state[0] = "1";
+        int head = 0;
+        while (true) {
+            String input = inputs.get(head);
+            int index = findRule(input, this.state[0]);
+            if (index == (-1)) {
+                break;
+            }
+            String[] description = this.descriptions[index].split("0");
+            this.state[0] = description[2];
+            head = inputs.replace(head, description[3]);
+            if (description[4].equals("1")) {
+                head--;
+                if (head < 0) {
+                    head = inputs.replace(head, "1");
+                }
+            } else {
+                head++;
+                if (head > inputs.getSize() - 1) {
+                    head = inputs.replace(head, "1");
+                }
+            }
+        }
+        if (state[0].equals("11")) {
+            System.out.println("Enter final state. Language accepted");
+        } else {
+            System.out.println("No transition rule found for the given input and state. HAULT!!!!");
+        }
 
     }
-    //print tapes
-    public void print(){
 
+    // print tapes
+    public void print() {
+        System.out.println("The description tape : " + Arrays.toString(this.descriptions) + "\n"
+                + "The inputs tape : " + Arrays.toString(this.inputs) + "\n"
+                + "The state tape " + Arrays.toString(this.state));
     }
-    /** 
+
+    /**
      * find specefic rules
      */
-    public int findRule(String s){
-
+    public int findRule(String input, String state) {
+        for (int i = 0; i < descriptions.length; i++) {
+            String[] description = this.descriptions[i].split("0");
+            if (state.equals(description[0]) && input.equals(description[1])) {
+                return i;
+            }
+        }
+        return (-1);
     }
-    
 
-    
 }
